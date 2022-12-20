9bdca707b8ac17eeef540b5916695f269f31e4cf
Caio Matheus dos Santos Abra <caioabra41@gmail.com>
2022-12-20 16:43:04 -0300

    full start

---
+++ src/js/index.js
@@ -0,0 +1,23 @@
+var btn = document.querySelector(".btn");
+var position;
+
+
+btn.addEventListener("mouseover", function () {
+    if ($('input').val().length === 0) {
+        position ? (position = 0) : (position = 5);
+        btn.style.transform = `translate(${position}em, 0px)`;
+        btn.style.transition = "all 0.5s ease";
+    }
+
+    if ($('input').val().length > 0) {
+        position ? (position = 0) : (position = 0);
+        // alert()
+    }
+
+
+
+});
+
+$(document).ready(function () {
+
+});
